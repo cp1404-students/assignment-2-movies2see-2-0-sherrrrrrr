c84df7f0f0d2abea81f1a73f8e78e4a0b36a4111
prac_02 <784522713@11.com>
2024-04-20 05:07:48 +0800

    0419

--- movie.py
+++ movie.py
@@ -5,18 +5,17 @@
 
 
 class Movie:
-    class Movie:
-        def __init__(self, title, year, category, watched=False):
-            self.title = title
-            self.year = year
-            self.category = category
-            self.watched = watched
+    def __init__(self, title, year, category, watched=False):
+        self.title = title
+        self.year = year
+        self.category = category
+        self.watched = watched
 
-        def mark_as_watched(self):
-            self.watched = True
+    def mark_as_watched(self):
+        self.watched = True
 
-        def mark_as_unwatched(self):
-            self.watched = False
+    def mark_as_unwatched(self):
+        self.watched = False
 
-        def __str__(self):
-            return f"{self.title} ({self.year}) - {self.category} Watched: {self.watched}"
+    def __str__(self):
+        return f"{self.title} ({self.year}) - {self.category} Watched: {self.watched}"
