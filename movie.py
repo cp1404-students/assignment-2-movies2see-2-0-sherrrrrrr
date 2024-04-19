fac016a45b80ef576be06cdbbe339fa5ad2d4c71
prac_02 <784522713@11.com>
2024-04-20 05:03:01 +0800

    0419

--- movie.py
+++ movie.py
@@ -5,5 +5,18 @@
 
 
 class Movie:
-    """..."""
-    pass
+    class Movie:
+        def __init__(self, title, year, category, watched=False):
+            self.title = title
+            self.year = year
+            self.category = category
+            self.watched = watched
+
+        def mark_as_watched(self):
+            self.watched = True
+
+        def mark_as_unwatched(self):
+            self.watched = False
+
+        def __str__(self):
+            return f"{self.title} ({self.year}) - {self.category} Watched: {self.watched}"
